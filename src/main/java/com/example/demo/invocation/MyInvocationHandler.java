a5d1c49ddbfd03ca8706948daae3d42ae7a13cf7
lujp <lujp@mingyuanyun.com>
2022-07-26 00:39:45 +0800

    fix:实现获取包装内的泛型

--- src/main/java/com/example/demo/invocation/MyInvocationHandler.java
+++ src/main/java/com/example/demo/invocation/MyInvocationHandler.java
@@ -1,11 +1,9 @@
 package com.example.demo.invocation;
 
+import org.springframework.aop.support.AopUtils;
 import org.springframework.util.StringUtils;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
 
 /**
  * 动态代理实现
@@ -27,7 +25,15 @@ public class MyInvocationHandler implements InvocationHandler {
             return method.invoke(this, args);
         }
         //TODO 怎样获取代理类中的泛型
-
+        //实现了获取被代理类对象中的泛型
+        System.out.println(AopUtils.getTargetClass(args[0]));
+        Type[] types = method.getGenericParameterTypes();
+        for (Type type : types) {
+            if (type instanceof ParameterizedType) {
+                Type rawType = ((ParameterizedType) type).getRawType();
+                System.out.println(rawType);
+            }
+        }
         String methodName = method.getName();
         if (StringUtils.pathEquals(methodName, "hello")) {
             System.out.println("hello hello");
